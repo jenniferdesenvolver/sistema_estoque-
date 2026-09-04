840e09e9b7275ccf918fba5d11a1b839fc681355
jenniferdesenvolver <abissal2025@gmail.com>
2026-09-03 23:48:00 -0300

    Fix typo in number input function name

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 st.title (" sistema estoque") 
 produto= st.text_input (" nome do produto ") 
-estoque= st.nunber_input ("quantidade no estoque", min_value=0, step=1) 
+estoque= st.number_input ("quantidade no estoque", min_value=0, step=1) 
