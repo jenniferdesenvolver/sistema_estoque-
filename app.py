f14522db82a897b9a0d2c5872e822ab4b29fefa4
jenniferdesenvolver <abissal2025@gmail.com>
2026-09-03 23:51:09 -0300

    Add price input for product in inventory system

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title (" sistema estoque") 
 produto= st.text_input (" nome do produto ") 
 estoque= st.number_input ("quantidade no estoque", min_value=0, step=1) 
+preco= st.number_input (" preço de venda (R$)", min_value=0.0, step=0.50) 
