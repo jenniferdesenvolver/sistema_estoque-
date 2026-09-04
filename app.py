97d9d1fde6783a3cf8ff3abc9a17fb8fcad5004a
jenniferdesenvolver <abissal2025@gmail.com>
2026-09-04 00:05:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ preco= st.number_input (" preço de venda (R$)", min_value=0.0, step=0.50)
 custo= st.number_input ("custo do produto (R$)", min_value=0.0, step=0.50) 
 compra= st.number_input ( " quantidade vendas", min_value=1, step=1) 
 if st.button (" realizar venda") :
-st.write (" venda after realizada com sucesso") 
+  st.write ("venda after realizada com sucesso") 
