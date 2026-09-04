e2fed6db60eec00f2bf8a4644d76aa87aa2e4a78
jenniferdesenvolver <abissal2025@gmail.com>
2026-09-04 00:10:05 -0300

    Update app title and input labels for sales system

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st 
-st.title (" sistema estoque") 
-produto= st.text_input (" nome do produto ") 
-estoque= st.number_input ("quantidade no estoque", min_value=0, step=1) 
-preco= st.number_input (" preço de venda (R$)", min_value=0.0, step=0.50) 
-custo= st.number_input ("custo do produto (R$)", min_value=0.0, step=0.50) 
-compra= st.number_input ( " quantidade vendas", min_value=1, step=1) 
-if st.button (" realizar venda") :
-  st.write ("venda after realizada com sucesso") 
+st.title (" SISTEMA AFTER") 
+produto= st.text_input (" PRODUTO: ") 
+estoque= st.number_input ("QUANTIDADE ESTOQUE ", min_value=0, step=1) 
+preco= st.number_input (" PREÇO DE VENDA (R$)", min_value=0.0, step=0.50) 
+custo= st.number_input (" CUSTO DO PRODUTO (R$)", min_value=0.0, step=0.50) 
+compra= st.number_input ( " QUANTIDADE VENDA", min_value=1, step=1) 
+if st.button (" REALIZAR VENDA ") :
+  st.write ("VENDA REALIZADA COM SUCESSO ") 
