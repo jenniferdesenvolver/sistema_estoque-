72d1a52cef3dbf591d72e42687ffd08bbe0b4294
jenniferdesenvolver <abissal2025@gmail.com>
2026-09-04 02:48:25 -0300

    Fix output format for stock display

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ if st.button (" CANCELAR VENDA " ) :
   st.write (" VENDA CANCELADA COM SUCESSO") 
 if compra <= estoque:
   estoque= estoque - compra
- st.write ("estoque", estoque) 
+ st.write ("estoque:", estoque) 
  else 
   st. write (" estoque indisponivel") 
