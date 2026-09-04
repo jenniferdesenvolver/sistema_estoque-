4be0d262cb3f416bf2a6181cd3d5e9a269481549
jenniferdesenvolver <abissal2025@gmail.com>
2026-09-04 00:03:19 -0300

    Fix syntax error in button click handling

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ estoque= st.number_input ("quantidade no estoque", min_value=0, step=1)
 preco= st.number_input (" preço de venda (R$)", min_value=0.0, step=0.50) 
 custo= st.number_input ("custo do produto (R$)", min_value=0.0, step=0.50) 
 compra= st.number_input ( " quantidade vendas", min_value=1, step=1) 
-if st.button (" realizar venda") 
+if st.button (" realizar venda") :
 st. write (" venda after realizada com sucesso") 
